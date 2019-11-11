10f0eba0d6704834a41e294fa90427a6ecfdb9fa
Rivikantha <rivikantha@gmail.com>
2019-11-11 12:50:35 +0530

    created UserEntry model...

--- acc_reg/models.py
+++ acc_reg/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 class AccReg(models.Model):
 
-	pass
+	id = models.IntegerField('id',unique=True, db_index=True, primary_key = True)
+	barcode = models.CharField('barcode',max_length=10,null=True,blank=True)
+	inv_folio_no = models.CharField('inventory folio number',max_length=10,null=True,blank=True)
+
+	def __str__(self):
+
+		return self.barcode
+	
--- user/models.py
+++ user/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+
 class User(AbstractUser):
 
 	group_no = models.IntegerField('group_no',null=True)
+
+
+class StockEntry(models.Model):
+
+	STATUS = [
+	
+		('D', 'Duplicate'),
+		('O', 'Other'),
+		('M', 'Missmatch'),
+	    
+	]
+
+	id = models.IntegerField('id',unique=True, db_index=True, primary_key = True)
+	barcode = models.CharField('barcode',max_length=10,null=True,blank=True)
+	status = models.CharField('status',max_length=1, choices=STATUS)
+	user = models.OneToOneField(User,on_delete=models.CASCADE,blank=True,default="")
+
+
